d7a340eb5e94bb4d3c56d65292d1940f042e4e23
虎子哥 <flashindream@gmail.com>
2023-01-06 13:30:47 +0800

    add encode

--- example.js
+++ example.js
@@ -1,8 +1,26 @@
 
 const binding = require('./build/Release/jpeg');
-const buffer = Buffer.from("ABC");
-const out = Buffer.alloc(3);
+const fs = require("fs")
+const rgb = [];
 const width = 1920;
 const height = 1080;
-let size = binding.encode(buffer, buffer.length, out, out.length, width, height);
+const quality = 75;
+for(let i=0;i<width;i++)
+{
+    for(let j=0;j<height;j++)
+    {
+        rgb.push(Math.floor(0xff*Math.random()));
+        rgb.push(Math.floor(0xff*Math.random()));
+        rgb.push(Math.floor(0xff*Math.random()));
+    }
+}
+const buffer = Buffer.from(rgb);    //Input RGB Data
+const out = Buffer.alloc(4*1024*1024); //Max File Size = 4MB
+console.time("encode");
+console.log("start");
+let size = binding.encode(buffer, buffer.length, out, out.length, width, height,quality);
+console.timeEnd("encode");
+let fd = fs.openSync("./tmp.jpg","w");
+fs.writeSync(fd,out,0,size);
+fs.closeSync(fd);
 console.log('binding.encode() =', out[0],out[1],size);
--- jpeg.cc
+++ jpeg.cc
@@ -1,7 +1,13 @@
 #include <nan.h>
+#include "toojpeg.h"
 using namespace Nan;  
 using namespace v8;
-
+uint8_t* outBuffer;
+int idx;
+void myOutput(unsigned char oneByte) { 
+    outBuffer[idx] = oneByte;
+    idx++;
+}
 NAN_METHOD(encode) {  
     
     v8::Isolate *isolate = Isolate::GetCurrent();
@@ -12,13 +18,12 @@ NAN_METHOD(encode) {
     unsigned int width = info[4]->Uint32Value(context).ToChecked();
     unsigned int height = info[5]->Uint32Value(context).ToChecked();
 
-    uint8_t* outBuffer = (uint8_t*) node::Buffer::Data(info[2]->ToObject(context).ToLocalChecked());
+    outBuffer = (uint8_t*) node::Buffer::Data(info[2]->ToObject(context).ToLocalChecked());
     unsigned int outBufferSize = info[3]->Uint32Value(context).ToChecked();
-
-    for(unsigned int i = 0; i < rgbBufferSize; i++ ) {
-        outBuffer[i] = 5;
-    }   
-    info.GetReturnValue().Set(outBufferSize);
+    unsigned int quality = info[6]->Uint32Value(context).ToChecked();
+    idx = 0;
+    TooJpeg::writeJpeg(myOutput,rgbBuffer,width,height,true,quality);
+    info.GetReturnValue().Set(idx);
 }
 
 NAN_MODULE_INIT(Init) {  
